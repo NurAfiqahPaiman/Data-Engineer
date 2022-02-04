8c1d8547a913aee91c7a7c0145dde6111c5c59f0
Nur Afiqah Paiman <nurafiqahpaiman86@gmai.com>
2022-02-04 18:01:03 +0800

    demo using variables

---
+++ Python/Basic-concept/3-variables.py
@@ -0,0 +1,21 @@
+# Assigning thing to variable
+
+name = "Afiqah"
+type(name) #this is tuple
+
+#storing number 53 inside variable "number"
+number = 52
+number = number + 5
+print(number)
+
+# adding number by increment 
+number += 4
+print(number)
+
+# substract number by reducing
+number -= 10
+print(number)
+
+# assign values to multiple variable in one line
+name1, age, color = "Nur", 26, "red"
+print(name1,age,color)
--- Python/README.md
+++ Python/README.md
@@ -2,4 +2,4 @@
 
 This folder is to showcase my excellent understanding and working knowledge of Python coding skills.
 
-I will be using Jupyter Notebook to do python coding.
+I will be using VS Code to do python coding.
