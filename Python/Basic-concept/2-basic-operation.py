fe9a082ed9ecd69a123365f40f44bfcc759dd449
Nur Afiqah Paiman <nurafiqahpaiman86@gmai.com>
2022-02-04 17:48:58 +0800

    basic operation / calculator

---
+++ Python/Basic-concept/2-basic-operation.py
@@ -0,0 +1,25 @@
+#the basic operation like addition, substraction etc.
+
+#Addition
+add = 143 + 54
+print(add)
+
+#Subtraction
+subs = 6934 - 543
+print(subs)
+
+#Division
+divi = 36 / 6
+print(divi)
+
+#To the power of
+pwr = 86**3
+print(pwr)
+
+#Floor division
+flod = 123 // 2
+print(flod)
+
+#Modulo
+modu = 123 % 2
+print(modu)
